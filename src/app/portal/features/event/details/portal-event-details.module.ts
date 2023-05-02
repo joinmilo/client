86d95301e9d4314c816b5fdd3a1e064aedba8be9
DOOMED2WIN <timon@codeschluss.de>
2023-05-02 12:38:43 +0200

    reworked details, carddetail description

--- src/app/portal/features/event/details/portal-event-details.module.ts
+++ src/app/portal/features/event/details/portal-event-details.module.ts
@@ -1,12 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-<<<<<<< Upstream, based on main
 import { MatDividerModule } from '@angular/material/divider';
-=======
-import { MatCardModule } from '@angular/material/card';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
->>>>>>> 3ce9e26 added event details
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
@@ -14,14 +9,11 @@ import { CoreModule } from 'src/app/core/core.module';
 import { CardModule } from 'src/app/shared/card/card.module';
 import { CommentModule } from 'src/app/shared/comment/comment.module';
 import { FormModule } from 'src/app/shared/form/form.module';
+import { SocialShareModule } from 'src/app/shared/share-button/social-share.module';
 import { TableModule } from 'src/app/shared/table/table.module';
 import { TitleModule } from 'src/app/shared/title/title.module';
 import { PortalEventCommentsComponent } from './components/comments/portal-event-comments.component';
 import { PortalEventDetailsComponent } from './components/details/portal-event-details.component';
-import { EventCardDetailComponent } from './components/event-card-detail/event-card-detail.component';
-import { EventDescriptionComponent } from './components/event-description/event-description.component';
-import { EventEvaluationComponent } from './components/event-evaluation/event-evaluation.component';
-import { EventParticipantsComponent } from './components/event-participants/event-participants.component';
 import { portalEventDetailsStateKey } from './constants/event-details.constant';
 import { PortalEventDetailsRoutingModule } from './portal-event-details-routing.module';
 import { PortalEventDetailsEffects } from './state/portal-event-details.effects';
@@ -49,6 +41,7 @@ const modules = [
   FormModule,
   TableModule,
   TitleModule,
+  SocialShareModule,
 ];
 
 const libs = [
