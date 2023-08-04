34631ccf1eb052e0e1b024d62fea547824f07db9
DOOMED2WIN <timon@codeschluss.de>
2023-08-04 16:02:31 +0200

    wip

--- src/app/portal/main/components/features/portal-page-features.component.ts
+++ src/app/portal/main/components/features/portal-page-features.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, Injector, Input, OnInit, ViewChild, ViewContainerRef, createNgModule } from '@angular/core';
 import { articlesFeatureKey, authorsFeatureKey, calendarFeatureKey, contestsFeatureKey, dealsFeatureKey, eventsFeatureKey, formsFeatureKey, guestArticlesFeatureKey, mapFeatureKey, mediaFeatureKey, organisationsFeatureKey, reportsFeatureKey, surveysFeatureKey } from 'src/app/core/constants/core.constants';
+
 import { Maybe, PageFeatureEntity } from 'src/schema/schema';
 
 @Component({
@@ -9,14 +10,14 @@ import { Maybe, PageFeatureEntity } from 'src/schema/schema';
 })
 export class PortalPageFeaturesComponent implements OnInit {
 
-  @ViewChild("featureComponents", {read: ViewContainerRef})
+  @ViewChild("featureComponents", { read: ViewContainerRef })
   featureComponents!: ViewContainerRef;
 
   @Input()
   public pageFeatures?: Maybe<Maybe<PageFeatureEntity>[]>;
 
   public features = {
-    articles: articlesFeatureKey,
+    articles: { articlesFeatureKey },
     authors: authorsFeatureKey,
     calendar: calendarFeatureKey,
     contests: contestsFeatureKey,
@@ -31,13 +32,15 @@ export class PortalPageFeaturesComponent implements OnInit {
     surveys: surveysFeatureKey,
   }
 
+  constructor(private injector: Injector) { }
+
   public ngOnInit(): void {
     this.pageFeatures = [...(this.pageFeatures || [])].sort((f1, f2) => (f1?.order || 0) - (f2?.order || 0));
 
     this.loadFeatures();
   }
 
-  async loadFeatures(){
+  async loadFeatures() {
 
     const { ArticlePageFeatureComponent } = await import("./../../../features/article/page-feature/component/article-page-feature.component");
     const { EventPageFeatureComponent } = await import("./../../../features/event/page-feature/component/event-page-feature.component");
@@ -51,45 +54,82 @@ export class PortalPageFeaturesComponent implements OnInit {
     const { FormPageFeatureComponent } = await import("./../../../features/form/page-feature/component/form-page-feature.component");
     const { GuestArticlePageFeatureComponent } = await import("./../../../features/guest-article/page-feature/component/guest-article-page-feature.component");
     const { MapPageFeatureComponent } = await import("./../../../features/map/page-feature/component/map-page-feature.component");
+
+    const { PortalArticlePageFeatureModule } = await import("src/app/portal/features/article/page-feature/portal-article-page-feature.module");
+    const articleModuleRef = createNgModule(PortalArticlePageFeatureModule, this.injector);
+
+    const { PortalMapPageFeatureModule } = await import("./../../../features/map/page-feature/portal-map-page-feature.module");
+    const mapModuleRef = createNgModule(PortalMapPageFeatureModule, this.injector);
+
+    const { PortalEventPageFeatureModule } = await import("src/app/portal/features/event/page-feature/portal-event-page-feature.module");
+    const eventModuleRef = createNgModule(PortalEventPageFeatureModule, this.injector);
+
+    const { PortalCalendarPageFeatureModule } = await import("src/app/portal/features/calendar/page-feature/portal-calendar-page-feature.module");
+    const calendarModuleRef = createNgModule(PortalCalendarPageFeatureModule, this.injector);
+
+    const { PortalReportPageFeatureModule } = await import("src/app/portal/features/report/page-feature/portal-report-page-feature.module");
+    const reportModuleRef = createNgModule(PortalReportPageFeatureModule, this.injector);
+
+    const { PortalAuthorPageFeatureModule } = await import("src/app/portal/features/author/page-feature/portal-author-page-feature.module");
+    const authorModuleRef = createNgModule(PortalAuthorPageFeatureModule, this.injector);
+
+    const { PortalOrganisationPageFeatureModule } = await import("src/app/portal/features/organisation/page-feature/portal-organisation-page-feature.module");
+    const organisationModuleRef = createNgModule(PortalOrganisationPageFeatureModule, this.injector);
+
+    const { PortalDealPageFeatureModule } = await import("src/app/portal/features/deal/page-feature/portal-deal-page-feature.module");
+    const dealModuleRef = createNgModule(PortalDealPageFeatureModule, this.injector);
+
+    const { PortalContestPageFeatureModule } = await import("src/app/portal/features/contest/page-feature/portal-contest-page-feature.module");
+    const contestModuleRef = createNgModule(PortalContestPageFeatureModule, this.injector);
+
+    const { PortalSurveyPageFeatureModule } = await import("src/app/portal/features/survey/page-feature/portal-survey-page-feature.module");
+    const surveyModuleRef = createNgModule(PortalSurveyPageFeatureModule, this.injector);
+
+    const { PortalFormPageFeatureModule } = await import("src/app/portal/features/form/page-feature/portal-form-page-feature.module");
+    const formModuleRef = createNgModule(PortalFormPageFeatureModule, this.injector);
+
+    const { PortalGuestArticlePageFeatureModule } = await import("src/app/portal/features/guest-article/page-feature/portal-guest-article-page-feature.module");
+    const guestArticleModuleRef = createNgModule(PortalGuestArticlePageFeatureModule, this.injector);
+
     this.featureComponents.clear();
 
     this.pageFeatures?.forEach(pageFeature => {
       switch (pageFeature?.feature?.key) {
         case 'articles':
-          this.featureComponents.createComponent(ArticlePageFeatureComponent);
+          this.featureComponents.createComponent(ArticlePageFeatureComponent, { ngModuleRef: articleModuleRef });
           break;
         case 'events':
-          this.featureComponents.createComponent(EventPageFeatureComponent);
+          this.featureComponents.createComponent(EventPageFeatureComponent, { ngModuleRef: eventModuleRef });
           break;
         case 'calendar':
-          this.featureComponents.createComponent(CalendarPageFeatureComponent);
+          this.featureComponents.createComponent(CalendarPageFeatureComponent, { ngModuleRef: calendarModuleRef });
           break;
         case 'reports':
-          this.featureComponents.createComponent(ReportPageFeatureComponent);
+          this.featureComponents.createComponent(ReportPageFeatureComponent, { ngModuleRef: reportModuleRef });
           break;
         case 'authors':
-          this.featureComponents.createComponent(AuthorPageFeatureComponent);
+          this.featureComponents.createComponent(AuthorPageFeatureComponent, { ngModuleRef: authorModuleRef });
           break;
         case 'organisations':
-          this.featureComponents.createComponent(OrganisationPageFeatureComponent);
+          this.featureComponents.createComponent(OrganisationPageFeatureComponent, { ngModuleRef: organisationModuleRef });
           break;
         case 'deals':
-          this.featureComponents.createComponent(DealPageFeatureComponent);
+          this.featureComponents.createComponent(DealPageFeatureComponent, { ngModuleRef: dealModuleRef });
           break;
         case 'contests':
-          this.featureComponents.createComponent(ContestPageFeatureComponent);
+          this.featureComponents.createComponent(ContestPageFeatureComponent, { ngModuleRef: contestModuleRef });
           break;
         case 'surveys':
-          this.featureComponents.createComponent(SurveyPageFeatureComponent);
+          this.featureComponents.createComponent(SurveyPageFeatureComponent, { ngModuleRef: surveyModuleRef });
           break;
         case 'forms':
-          this.featureComponents.createComponent(FormPageFeatureComponent);
+          this.featureComponents.createComponent(FormPageFeatureComponent, { ngModuleRef: formModuleRef });
           break;
         case 'guestArticle':
-          this.featureComponents.createComponent(GuestArticlePageFeatureComponent);
+          this.featureComponents.createComponent(GuestArticlePageFeatureComponent, { ngModuleRef: guestArticleModuleRef });
           break;
         case 'map':
-          this.featureComponents.createComponent(MapPageFeatureComponent);
+          this.featureComponents.createComponent(MapPageFeatureComponent, { ngModuleRef: mapModuleRef });
           break;
         default:
           break;
--- src/app/portal/main/portal-main.module.ts
+++ src/app/portal/main/portal-main.module.ts
@@ -8,15 +8,13 @@ import { StoreModule } from '@ngrx/store';
 import { CoreModule } from 'src/app/core/core.module';
 import { TitleImageComponent } from 'src/app/shared/image/title/title-image.component';
 import { MediaModule } from 'src/app/shared/media/media.module';
-import { PortalArticlePageFeatureModule } from '../features/article/page-feature/portal-article-page-feature.module';
 import { PortalAuthorPageFeatureModule } from '../features/author/page-feature/portal-author-page-feature.module';
 import { PortalCalendarPageFeatureModule } from '../features/calendar/page-feature/portal-calendar-page-feature.module';
 import { PortalContestPageFeatureModule } from '../features/contest/page-feature/portal-contest-page-feature.module';
 import { PortalDealPageFeatureModule } from '../features/deal/page-feature/portal-deal-page-feature.module';
 import { PortalEventPageFeatureModule } from '../features/event/page-feature/portal-event-page-feature.module';
 import { PortalFormPageFeatureModule } from '../features/form/page-feature/portal-form-page-feature.module';
-import { PortalGuestArticlePageFeatureModule } from '../features/guest-article/page-feature/guest-portal-article-page-feature.module';
-import { PortalMapPageFeatureModule } from '../features/map/page-feature/portal-map-page-feature.module';
+import { PortalGuestArticlePageFeatureModule } from '../features/guest-article/page-feature/portal-guest-article-page-feature.module';
 import { PortalOrganisationPageFeatureModule } from '../features/organisation/page-feature/portal-organisation-page-feature.module';
 import { PortalReportPageFeatureModule } from '../features/report/page-feature/portal-report-page-feature.module';
 import { PortalSurveyPageFeatureModule } from '../features/survey/page-feature/portal-survey-page-feature.module';
@@ -35,7 +33,7 @@ const components = [
 ];
 
 const features = [
-  PortalArticlePageFeatureModule,
+  // PortalArticlePageFeatureModule,
   PortalAuthorPageFeatureModule,
   PortalCalendarPageFeatureModule,
   PortalContestPageFeatureModule,
@@ -43,7 +41,7 @@ const features = [
   PortalEventPageFeatureModule,
   PortalFormPageFeatureModule,
   PortalGuestArticlePageFeatureModule,
-  PortalMapPageFeatureModule,
+  // PortalMapPageFeatureModule,
   PortalOrganisationPageFeatureModule,
   PortalReportPageFeatureModule,
   PortalSurveyPageFeatureModule,
