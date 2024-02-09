cb7f7493483735f9aa366c1b5f087f5a8dae12e7
PRiedmiller <peter@codeschluss.de>
2024-02-09 09:12:13 +0100

    deleted unnecessary query properties

--- src/app/admin/api/generated/get-plugins.query.generated.ts
+++ src/app/admin/api/generated/get-plugins.query.generated.ts
@@ -10,7 +10,7 @@ export type GetPluginsQueryVariables = Types.Exact<{
 }>;
 
 
-export type GetPluginsQuery = { __typename?: 'Query', getPlugins?: { __typename?: 'PageableList_PluginEntity', total: any, result?: Array<{ __typename?: 'PluginEntity', id?: string | null, active?: boolean | null, code?: string | null, icon?: string | null, name?: string | null, released?: boolean | null, menuItems?: Array<{ __typename?: 'MenuItemEntity', id?: string | null, header?: boolean | null, name?: string | null, order?: number | null, icon?: string | null, parent?: { __typename?: 'MenuItemEntity', id?: string | null, name?: string | null } | null } | null> | null, translatables?: Array<{ __typename?: 'PluginTranslatableEntity', id?: string | null, name?: string | null, language?: { __typename?: 'LanguageEntity', id?: string | null, locale?: string | null, name?: string | null } | null } | null> | null } | null> | null } | null };
+export type GetPluginsQuery = { __typename?: 'Query', getPlugins?: { __typename?: 'PageableList_PluginEntity', total: any, result?: Array<{ __typename?: 'PluginEntity', id?: string | null, active?: boolean | null, code?: string | null, icon?: string | null, name?: string | null, released?: boolean | null, menuItems?: Array<{ __typename?: 'MenuItemEntity', id?: string | null } | null> | null, translatables?: Array<{ __typename?: 'PluginTranslatableEntity', id?: string | null, name?: string | null, language?: { __typename?: 'LanguageEntity', id?: string | null, locale?: string | null, name?: string | null } | null } | null> | null } | null> | null } | null };
 
 export const GetPluginsDocument = gql`
     query getPlugins($params: FilterSortPaginateInput) {
@@ -24,14 +24,6 @@ export const GetPluginsDocument = gql`
       released
       menuItems {
         id
-        header
-        name
-        order
-        icon
-        parent {
-          id
-          name
-        }
       }
       translatables {
         id
